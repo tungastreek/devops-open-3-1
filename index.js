ac1957b4aa917bfd8f7501c6d96b6162f0996aa0
TungLe <tung@astreek.com>
2025-12-12 10:53:21 +0700

    Take 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World, Take 1!</h1>')
+  res.send('<h1>Hello World, Take 3!</h1>')
 })
 
 const PORT = 8080
